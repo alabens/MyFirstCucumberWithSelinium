a2b132aa2d68515306f51069dee7f887f784a585
alabens <ala.bensalah@ensi-uma.tn>
2020-04-17 21:44:13 +0100

    adding testng.xml

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,24 +1,33 @@
 package Runner;
+
+import com.cucumber.listener.ExtentProperties;
+import com.cucumber.listener.Reporter;
+import cucumber.api.formatter.Formatter;
+import io.cucumber.core.options.RuntimeOptions;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 //@RunWith(Cucumber.class)
 @CucumberOptions(
-        features=("src/test/java/Features"),
-        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty" },
-        glue="Steps")
+        monochrome = true,
+        features = ("src/test/java/Features"),
+        plugin = {"json:target/cucumber.json",
+                "html:target/site/cucumber-pretty",
+                //"com.cucumber.listener.ExtentCucumberFormatter:target/Extent/ExtentReport.html"
+        },
+        glue = "Steps")
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 
-    @Override
-    @DataProvider
-    //@DataProvider (parallel = true) -- For parallel execution support (which is not going to work for our code)
-    public Object[][] scenarios() {
-        return super.scenarios();
     }
-
-}
+//}
